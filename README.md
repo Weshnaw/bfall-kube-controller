774e048f8e24e62098149087fa07c33a5896281b
Brendon Fallquist <bfallquist@gmail.com>
2026-02-19 11:42:12 -0500

    refactor shared lib to create a dynamic leader election controller

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 - ingress-controller: create a tailscale ingress from a service based on a couple tags
 
 # TODO:
-- flesh out the controller deployment
-- refactor when/if I decide to add more controllers (give each controller it's own example, deployment, and docker package)
-  - move generic initialization logic to shared lib
-  - move leader election logic to shared lib
+- considering: creating macros for the reconciler and error policy traits
--- tailscale-ingress-controller/src/main.rs
+++ tailscale-ingress-controller/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::BTreeMap,
+    pin::Pin,
     sync::{
         Arc,
         atomic::{AtomicBool, Ordering},
@@ -7,8 +8,6 @@ use std::{
     time::Duration,
 };
 
-use derive_more::{Debug, Display, Error, From};
-use futures::StreamExt;
 use k8s_openapi::api::{
     core::v1::Service,
     networking::v1::{
@@ -19,46 +18,22 @@ use kube::{
     Api, Client, Resource,
     api::{DeleteParams, ListParams, ObjectMeta, Patch, PatchParams},
     runtime::{
-        Controller,
         controller::{Action, Config},
         reflector::Lookup,
-        watcher,
     },
 };
-use kube_leader_election::{LeaseLock, LeaseLockParams, LeaseLockResult};
-use metrics::{counter, gauge, histogram};
-// use metrics_dashboard::{ChartType, DashboardOptions, build_dashboard_route};
-// use poem::{EndpointExt, Route, Server, listener::TcpListener, middleware::Tracing};
-use tokio::{
-    sync::{Mutex, mpsc},
-    time::Instant,
-};
-use tracing::{debug, info, trace, warn};
-use tracing_subscriber::{EnvFilter, fmt, prelude::*};
-use uuid::Uuid;
+use metrics::{counter, histogram};
+use shared::controller::{BFallController, CheckLeadershipStatus};
+use tokio::time::Instant;
+use tracing::{debug, info, warn};
 
 pub mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
-struct Data {
-    client: Client,
-    leader_status: Arc<AtomicBool>,
-}
-
-#[derive(Debug, Display, Error, From)]
-enum AppError {
-    KubeError(kube::Error),
-    LeaderElectionError(kube_leader_election::Error),
-    #[from(skip)]
-    MissingObjectKey(#[error(not(source))] &'static str),
-    #[from(skip)]
-    CouldNotCreateResource(#[error(not(source))] &'static str),
-}
-
 #[tracing::instrument(level = "debug", skip(ctx, svc), fields(svc.name = svc.metadata.name, svc.namespace=svc.metadata.namespace))]
-async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError> {
-    if !ctx.leader_status.load(Ordering::Relaxed) {
+async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, shared::Error> {
+    if !ctx.is_leader() {
         debug!("Skipping reconciliation as not leader");
         return Ok(Action::requeue(Duration::from_secs(30)));
     }
@@ -69,17 +44,17 @@ async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError
         .metadata
         .namespace
         .as_ref()
-        .ok_or(AppError::MissingObjectKey("metadata.namespace"))?;
+        .ok_or(shared::Error::MissingObjectKey("metadata.namespace"))?;
     let name = svc
         .metadata
         .name
         .as_ref()
-        .ok_or(AppError::MissingObjectKey("metadata.name"))?;
+        .ok_or(shared::Error::MissingObjectKey("metadata.name"))?;
     let service_uid = svc
         .metadata
         .uid
         .as_ref()
-        .ok_or(AppError::MissingObjectKey("metadata.uid"))?;
+        .ok_or(shared::Error::MissingObjectKey("metadata.uid"))?;
     let mut ingress_name = format!("tsi-{name}");
     let ingress_api = Api::<Ingress>::namespaced(client.clone(), namespace);
     if let Some(labels) = &svc.metadata.labels
@@ -91,7 +66,7 @@ async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError
 
         let owner_ref = svc
             .controller_owner_ref(&())
-            .ok_or(AppError::MissingObjectKey("metadata.owner_references"))?;
+            .ok_or(shared::Error::MissingObjectKey("metadata.owner_references"))?;
 
         let existing = ingress_api
             .list(&ListParams::default().labels(&format!(
@@ -107,12 +82,12 @@ async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError
                     .metadata
                     .name
                     .clone()
-                    .ok_or(AppError::MissingObjectKey("metadata.name"))?,
+                    .ok_or(shared::Error::MissingObjectKey("metadata.name"))?,
                 ingress
                     .metadata
                     .namespace
                     .clone()
-                    .ok_or(AppError::MissingObjectKey("metadata.name"))?,
+                    .ok_or(shared::Error::MissingObjectKey("metadata.name"))?,
             )
         } else {
             info!("Creating tailscale ingress: port:{port} should_funnel:{should_funnel}");
@@ -133,7 +108,7 @@ async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError
 
             if !found_usable_name {
                 warn!("Failed to find a usable name for the ingress");
-                return Err(AppError::CouldNotCreateResource(
+                return Err(shared::Error::CouldNotCreateResource(
                     "Failed to find a usable name for the ingress",
                 ));
             }
@@ -235,125 +210,67 @@ async fn reconcile(svc: Arc<Service>, ctx: Arc<Data>) -> Result<Action, AppError
 
 /// The controller triggers this on reconcile errors
 #[tracing::instrument(level = "warn", skip(_ctx, _svc))]
-fn error_policy(_svc: Arc<Service>, e: &AppError, _ctx: Arc<Data>) -> Action {
+fn error_policy(_svc: Arc<Service>, e: &shared::Error, _ctx: Arc<Data>) -> Action {
     warn!("Reconcile error: {}", e);
     counter!("reconciled_error").increment(1);
     Action::requeue(Duration::from_secs(1))
 }
 
+struct Data {
+    client: Client,
+    leader_status: Option<Arc<AtomicBool>>,
+}
+
+impl CheckLeadershipStatus for Data {
+    fn is_leader(&self) -> bool {
+        self.leader_status
+            .as_ref()
+            .is_some_and(|status| status.load(Ordering::Relaxed))
+    }
+
+    fn set_leader_atomic_bool(&mut self, status: Arc<AtomicBool>) {
+        self.leader_status = Some(status);
+    }
+}
+
+struct Reconciler;
+
+impl shared::controller::Reconciler<Service, Data> for Reconciler {
+    type ReconcilerFut =
+        Pin<Box<dyn Future<Output = Result<Action, shared::Error>> + Send + 'static>>;
+    fn reconcile(key: Arc<Service>, context: Arc<Data>) -> Self::ReconcilerFut {
+        Box::pin(reconcile(key, context))
+    }
+}
+
+struct ErrorPolicy;
+
+impl shared::controller::ErrorPolicy<Service, shared::Error, Data> for ErrorPolicy {
+    fn error_policy(key: Arc<Service>, error: &shared::Error, context: Arc<Data>) -> Action {
+        error_policy(key, error, context)
+    }
+}
 #[tokio::main]
-async fn main() -> Result<(), AppError> {
+async fn main() -> Result<(), shared::Error> {
     if std::env::var("RUST_LOG").is_err() {
-        // We are just setting a default RUST_LOG value
+        // We are just setting a default RUST_LOG value race conditions don't really matter here
         unsafe {
             std::env::set_var("RUST_LOG", "warn,tailscale_ingress_controller=info");
         }
     }
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(EnvFilter::from_default_env())
-        .init();
-
-    info!("Initializing client...");
 
     let client = Client::try_default().await?;
-
-    let lease_namespace = std::env::var("NAMESPACE").unwrap_or("default".into());
-
-    let (leader_tx, leader_rx) = mpsc::channel(10);
-    let leader_status = Arc::new(AtomicBool::new(false));
-    let leader_guage = gauge!("leader_status");
-    leader_guage.set(0);
-
-    let uuid = Uuid::new_v4().to_string();
-    let leadership = Arc::new(LeaseLock::new(
-        client.clone(),
-        &lease_namespace,
-        LeaseLockParams {
-            holder_id: uuid.clone(),
-            lease_name: "tailscale-ingress-controller-lock".into(),
-            lease_ttl: Duration::from_secs(15),
-        },
-    ));
-    let leadership_clone = leadership.clone();
-    let status_clone = leader_status.clone();
-    tokio::spawn(async move {
-        let leadership = leadership_clone;
-        let status = status_clone;
-        info!("Starting leader election uuid={uuid}...");
-        loop {
-            match leadership.try_acquire_or_renew().await {
-                Ok(LeaseLockResult::Acquired(_)) => {
-                    debug!("Lease acquired...");
-                    leader_guage.set(1);
-                    status.store(true, Ordering::SeqCst);
-                    leader_tx.try_send(true).ok();
-                }
-                Ok(lease) => {
-                    debug!("Unable to acquire lease...\n{:?}", lease);
-                    leader_guage.set(0);
-                    status.store(false, Ordering::SeqCst);
-                    leader_tx.try_send(false).ok();
-                }
-                Err(e) => {
-                    warn!("failed to acquire or renew leadership: {}", e);
-                    leader_guage.set(0);
-                    status.store(false, Ordering::SeqCst);
-                    leader_tx.try_send(false).ok();
-                }
-            }
-            tokio::time::sleep(Duration::from_secs(10)).await;
-        }
-    });
-
-    let should_end_loop = Arc::new(AtomicBool::new(true));
-    let leader_rx = Arc::new(Mutex::new(leader_rx));
-    loop {
-        let client = client.clone();
-        let leader_status = leader_status.clone();
-        let config = Config::default().debounce(Duration::from_secs(5));
-        let svc = Api::<Service>::all(client.clone());
-        let ingress = Api::<Ingress>::all(client.clone());
-
-        while !leader_rx.lock().await.recv().await.unwrap_or(false) {
-            trace!("Waiting for status notification...");
-        }
-
-        info!("Starting controller...");
-        let should_end_loop_clone = should_end_loop.clone();
-        let leader_rx_clone = leader_rx.clone();
-        Controller::new(svc, watcher::Config::default())
-            .owns(ingress, watcher::Config::default())
-            .with_config(config)
-            .shutdown_on_signal()
-            .graceful_shutdown_on(async move {
-                while leader_rx_clone.lock().await.recv().await.unwrap_or(false) {}
-                debug!("Leadership lost, restarting controller...");
-                should_end_loop_clone.clone().store(false, Ordering::SeqCst);
-            })
-            .run(
-                reconcile,
-                error_policy,
-                Arc::new(Data {
-                    client,
-                    leader_status,
-                }),
-            )
-            .for_each(|res| async move {
-                match res {
-                    Ok(o) => trace!("reconciled {:?}", o),
-                    Err(e) => trace!("reconcile failed: {}", e),
-                }
-            })
-            .await;
-
-        if should_end_loop.load(Ordering::SeqCst) {
-            break;
-        }
-        should_end_loop.clone().store(true, Ordering::SeqCst);
-    }
-    info!("Controller terminated...");
-    leadership.step_down().await?;
+    let config = Config::default().debounce(Duration::from_secs(5));
+    let svc = Api::<Service>::all(client.clone());
+    let ingress = Api::<Ingress>::all(client.clone());
+    BFallController::new(client.clone(), config, svc)
+        .await
+        .owns(ingress)
+        .run::<Reconciler, ErrorPolicy, Data>(Data {
+            client,
+            leader_status: None,
+        })
+        .await?;
 
     Ok(())
 }
