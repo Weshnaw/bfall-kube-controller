4c996ab2289f2eadd58ec801273a5376d2d0614e
Brendon Fallquist <bfallquist@gmail.com>
2026-06-04 15:46:08 -0400

    minor updates

--- pangolin-gateway-controller/src/http_route/sync.rs
+++ pangolin-gateway-controller/src/http_route/sync.rs
@@ -1,6 +1,7 @@
 use crate::intermediate::RetrievedData;
 
 pub fn update_pangolin_api(data: &RetrievedData) -> Result<(), shared::Error> {
+    // TODO: consider using the blueprint API, then the validation is handled by pangolin
     for (hostname, rule) in data.combined_iter() {
         hostname.apply_rule(rule)?;
     }
